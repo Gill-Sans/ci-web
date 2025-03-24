f0916fefc207513b91b1835d81feff39199515b7
gill-sans <mertensgill@outlook.com>
2025-03-24 01:41:53 +0100

    feat: integrate Keycloak for authentication with silent SSO check

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,38 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {
+    INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+    includeBearerTokenInterceptor,
+    KeycloakService,
+    provideKeycloak
+} from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideKeycloak({
+        config: {
+            url: 'http://localhost:8180/auth',
+            realm: 'Capit',
+            clientId: 'angular-client'
+        },
+        initOptions: {
+            onLoad: 'login-required',
+            silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html'
+        }
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
+    {
+        provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+        useValue: [
+            {
+                urlPattern: /^http:\/\/localhost:9000\/.*$/,
+                httpMethods: ['GET', 'POST', 'PUT', 'DELETE']
+            }
+        ]
+    }
   ]
 };
