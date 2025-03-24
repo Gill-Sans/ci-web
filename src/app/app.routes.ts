0ff3a6179c11bac519251cf6cc7d1822d69bcba0
gill-sans <mertensgill@outlook.com>
2025-03-24 01:43:09 +0100

    feat: update routing to include WelcomeComponent and authentication guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
-import { Routes } from '@angular/router';
-import {SessionsOverviewComponent} from './feature/sessions-overview/sessions-overview.component';
+import {ActivatedRouteSnapshot, Route, RouterStateSnapshot, Routes} from '@angular/router';
+import {SessionsOverviewComponent} from './feature/sessions/sessions-overview/sessions-overview.component';
+import {WelcomeComponent} from './feature/public/welcome/welcome.component';
+import {AuthGuardData, createAuthGuard} from 'keycloak-angular';
+
+const isUserAuthenticated = async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, authData: AuthGuardData) => {
+   const { authenticated } = authData;
+   return authenticated;
+};
+
+const isUserAdmin = async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, authData: AuthGuardData) => {
+    const { authenticated, grantedRoles } = authData;
+    return authenticated && grantedRoles.realmRoles.includes('admin');
+};
 
 export const routes: Routes = [
-  { path: "", component: SessionsOverviewComponent },
-  { path: "sessions", component: SessionsOverviewComponent },
+  { path: "", component: WelcomeComponent },
+  { path: "sessions", component: SessionsOverviewComponent, canActivate: [createAuthGuard(isUserAuthenticated)] }
 ];
