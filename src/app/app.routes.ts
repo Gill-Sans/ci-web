500cd3b7f84e3cc26279288cc81e883d03b4f53b
gill-sans <mertensgill@outlook.com>
2025-03-28 07:47:26 +0100

    feat: add conference routing and components to platform layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {WelcomeComponent} from './feature/public/welcome/welcome.component';
 import {AuthGuardData, createAuthGuard} from 'keycloak-angular';
 import {PlatformLayoutComponent} from './feature/layouts/platform-layout/platform-layout.component';
 import {PublicLayoutComponent} from './feature/layouts/public-layout/public-layout.component';
+import {ConferenceCreateComponent} from './feature/conferences/conference-create/conference-create.component';
+import {ConferenceOverviewComponent} from './feature/conferences/conference-overview/conference-overview.component';
 
 const isUserAuthenticated = async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, authData: AuthGuardData) => {
    const { authenticated } = authData;
@@ -28,7 +30,9 @@ export const routes: Routes = [
         component: PlatformLayoutComponent,
         canActivate: [createAuthGuard(isUserAuthenticated)],
         children: [
-            { path: "sessions", component: SessionsOverviewComponent }
+            { path: "sessions", component: SessionsOverviewComponent },
+            { path: "conferences", component: ConferenceOverviewComponent },
+            { path: "conferences/create", component: ConferenceCreateComponent }
         ]
     }
 ];
---
+++ src/app/feature/conferences/conference-overview/conference-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conference-overview',
+  imports: [],
+  templateUrl: './conference-overview.component.html',
+  styleUrl: './conference-overview.component.scss'
+})
+export class ConferenceOverviewComponent {
+
+}
