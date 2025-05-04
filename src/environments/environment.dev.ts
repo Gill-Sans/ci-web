fb4f83d3b548eb987fde9242dca0fc2c5801a203
gill-sans <mertensgill@outlook.com>
2025-05-04 13:03:05 +0200

    feat: enhance environment configurations for local and development setups with file replacements and API URLs

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    KEYCLOAK_URL: 'http://129.168.0.53:8180',
+    BASE_API_URL: 'http://129.168.0.56:9001',
+    BASE_API_URL_SHORT: '129.168.0.56:9001',
+}
