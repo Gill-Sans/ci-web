bc60340e4d882814e87185d2231b25ba14a7b7e2
gill-sans <mertensgill@outlook.com>
2025-05-28 03:31:30 +0200

    feat: increase delay in conference creation navigation for better user experience

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     keycloak: {
-        realm: 'capit',
-        clientId: 'angular-client',
-        url: 'https://capit-dev.keycloak.mertenshome.com',
-        postLogoutRedirectUri: 'https://capit-dev.mertenshome.com/logout',
+        realm: "capit",
+        clientId: "angular-client",
+        url: "https://capit-dev.keycloak.mertenshome.com",
+        postLogoutRedirectUri: "https://capit-dev.mertenshome.com/logout",
     },
     backend: {
-        url: 'https://capit-dev.gateway.mertenshome.com',
-        url_domain: 'capit-dev.gateway.mertenshome.com'
+        url: "https://capit-dev.gateway.mertenshome.com",
+        url_domain: "capit-dev.gateway.mertenshome.com"
     }
 }
